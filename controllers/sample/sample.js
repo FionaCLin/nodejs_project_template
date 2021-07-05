89340731a9809fa6a840d89be0667a38f4ca3fac
FionaLin <fiona.cy.lin@gmail.com>
2021-07-05 22:56:06 +1000

    Linting

--- controllers/sample/sample.js
+++ controllers/sample/sample.js
@@ -3,6 +3,7 @@ import {sample as sampleLibrary} from '../../lib/sample/index.js';
 export default async function sample(request, response, next) {
   try {
     const {input: query = ''} = request.query;
+
     const responseString = await sampleLibrary({query});
     response.json(`${responseString}`);
   } catch (error) {
